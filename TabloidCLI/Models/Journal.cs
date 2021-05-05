0e9f0d720ce1bb3a62b7fc205d1dfc3ff51d5826
Jon Newton <jonn@hey.com>
2021-05-05 15:48:53 -0400

    Updated Journal Models and Repositories with class and methods to complete ticket #3

---
+++ TabloidCLI/Models/Journal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TabloidCLI.Models
+{
+    /// <summary>
+    ///     Custom type to create Journal Entry objects.
+    /// </summary>
+
+    public class Journal
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateDateTime { get; set; }
+
+        public List<Tag> Tags { get; set; } = new List<Tag>();
+
+    }
+}

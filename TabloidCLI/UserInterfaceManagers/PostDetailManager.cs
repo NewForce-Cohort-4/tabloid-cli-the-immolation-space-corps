26a747b91157b35f5070e46be7960ce7decfbbc9
Jon Newton <jonn@hey.com>
2021-05-11 10:20:23 -0400

    Started implentation on Delete Tags for Posts.

--- TabloidCLI/UserInterfaceManagers/PostDetailManager.cs
+++ TabloidCLI/UserInterfaceManagers/PostDetailManager.cs
@@ -117,7 +117,25 @@ namespace TabloidCLI.UserInterfaceManagers
 
         private void RemoveTag()
         {
-            throw new NotImplementedException();
-        }
+												Post post = _postRepository.Get(_postId);
+
+												Console.WriteLine($"Which tag would you like to remove from Post: {post.Title}?");
+												List<Tag> tags = post.Tags;
+
+												for (int i = 0; i < tags.Count; i++)
+												{
+																Tag tag = tags[i];
+																Console.WriteLine($" {i + 1}) {tag.Name}");
+												}
+												Console.Write("> ");
+
+												string input = Console.ReadLine();
+												try
+												{
+																int choice = int.Parse(input);
+																Tag tag = tags[choice - 1];
+																_postRepository
+												}
+								}
     }
 }
